56f13052891c46f506c5b6e75950d22525eade0f
Ethan Quinlan <ethan.quinlan2016@gmail.com>
2023-03-07 16:39:14 -0500

    added logic for editor.js

--- Develop/client/src/js/editor.js
+++ Develop/client/src/js/editor.js
@@ -6,7 +6,7 @@ export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check to see if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
@@ -23,7 +23,6 @@ export default class {
     });
 
     // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
@@ -33,7 +32,8 @@ export default class {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+
+    // Content will save when the editor 'loses focus'
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
