12f68175954e89158ec73fe834272082fdc042ab
Ethan Quinlan <ethan.quinlan2016@gmail.com>
2023-03-07 20:19:10 -0500

    added logic for service worker

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -24,7 +24,18 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+// TODO: Implement asset caching
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Set up asset cache
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
